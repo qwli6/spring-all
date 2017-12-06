a144feb863c29ecf0a4be5ee82c9cee67344fb7c
李启文 <selfassu@gmail.com>
2017-12-06 15:48:39 +0800

    SpringBoot 整合 Thymeleaf 模板引擎

--- Chapter2-1/src/main/java/com/itqiwen/chapter21/controller/HelloController.java
+++ Chapter2-1/src/main/java/com/itqiwen/chapter21/controller/HelloController.java
@@ -1,4 +1,48 @@
 package com.itqiwen.chapter21.controller;
 
+import com.itqiwen.chapter21.domain.User;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Controller
 public class HelloController {
+
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello(Model model){
+        User user = new User();
+        user.setId(1L);
+        user.setName("小华");
+        user.setSex("男");
+        User user2 = new User();
+        user2.setId(2L);
+        user2.setName("小红");
+        user2.setSex("男");
+        User user3 = new User();
+        user3.setId(3L);
+        user3.setName("小蓝");
+        user3.setSex("女");
+        List<User> userList = new ArrayList<>();
+        userList.add(user);
+        userList.add(user2);
+        userList.add(user3);
+        model.addAttribute("name", "小华");
+
+        model.addAttribute("userList", userList);
+        return "hello";
+    }
 }
--- Chapter2-1/src/main/java/com/itqiwen/chapter21/domain/User.java
+++ Chapter2-1/src/main/java/com/itqiwen/chapter21/domain/User.java
@@ -1,4 +1,40 @@
 package com.itqiwen.chapter21.domain;
 
 public class User {
+    private Long id;
+    private String name;
+    private String sex;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
 }
