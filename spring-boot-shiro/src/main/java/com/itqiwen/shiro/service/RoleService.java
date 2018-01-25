495a30d4f946e22c1a5b08a2c516e203bcbc9b4a
李启文 <selfassu@gmail.com>
2018-01-25 15:49:32 +0800

    移除无用资源

--- spring-boot-shiro/src/main/java/com/itqiwen/shiro/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.itqiwen.shiro.service;
-
-import com.itqiwen.shiro.domain.Role;
-
-import java.util.List;
-
-public interface RoleService {
-    List<Role> findByUserId(String username);
-}
--- spring-boot-shiro/src/main/java/com/itqiwen/shiro/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.itqiwen.shiro.service;
-
-import com.itqiwen.shiro.domain.User;
-
-public interface UserService {
-    User findByLoginName(String currentLoginName);
-
-}
